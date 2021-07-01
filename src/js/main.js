0795fa6811c5acd0f59d59187aac41f6047b615f
Anastasya <aamokeeva@yandex.ru>
2021-07-01 18:27:54 +0300

    Реализует простую подгрузку элементов, присутствующих в верстке

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ import sliders from "./modules/sliders";
 import forms from "./modules/forms";
 import mask from "./modules/mask";
 import validateInputs from "./modules/validate";
+import showMoreStyles from "./modules/showMoreStyles";
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
@@ -14,4 +15,5 @@ window.addEventListener('DOMContentLoaded', () => {
     mask('[name="phone"]');
     validateInputs('[name="name"]');
     validateInputs('[name="message"]');
+    showMoreStyles('.button-styles', '.styles-2');
 });
